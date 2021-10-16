c07d333f56f5fcf16e1b01e297d86ca12a7a5410
Livin R <liviin651@gmail.com>
2021-10-15 21:18:04 -0500

    initial load

---
+++ Routes/Auth_route.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const createError = require('http-errors');
+
+const User = require('../Models/User_model');
+const { authSchema } = require('../helpers/validation_schema');
+const { signAccessToken } = require('../helpers/jwt_helper')
+
+router.post('/register', async(req, res, next) =>{
+    // console.log(req.body)
+    try {
+
+        // const { email, password } = req.body
+        // if (!email || !password) {
+        //     throw createError.BadRequest()
+        // }
+        const result = await authSchema.validateAsync(req.body)
+        
+        const doesExist = await User.findOne({ email: result.email });
+        if (doesExist){
+            throw createError.Conflict(`${result.email} is alredy been registered`)
+        }
+
+        const user = new User( result );
+        console.log(user)
+        const saveUser = await user.save();
+        const accessToken = await signAccessToken(saveUser.id)
+        res.send(accessToken);
+
+    } catch (error) {
+        if( error.isJoi === true ) error.status = 422
+        next(error)
+    }
+});
+
+router.post('/login', async(req, res, next) =>{
+    res.send("login route")
+});
+
+router.post('/refresh-token', async(req, res, next) =>{
+    res.send("refresh token route")
+});
+
+router.delete('/logout', async(req, res, next) =>{
+    res.send("logout route")
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
